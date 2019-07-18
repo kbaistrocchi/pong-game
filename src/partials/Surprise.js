6b64b3731472b8b81672788b65cf38d871a11acf
Kayla <kaynuhe@gmail.com>
2019-07-18 09:46:14 -0400

    add: pseudo code for Surprise game upgrades

---
+++ src/partials/Surprise.js
@@ -0,0 +1,18 @@
+import {SVG_NS} from '../settings'
+
+// Add svg shapes on board. When ball hits them, games elements change.
+// Red >> paddles shrink
+// Green >> ball slows down
+// Yellow >> resets game play
+// Purple >> winning player looses 2 points
+
+
+// create svgs of different colours
+
+// create event listener or condition to trigger svg appearance (score?)
+
+// have svg appear at random position on board (not behind paddles)
+
+// condition for if ball hits new svg, trigger changes
+
+// PURPLE
